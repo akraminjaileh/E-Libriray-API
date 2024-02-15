46c77d8a8e1c6d563d5be832d7391c328d2794d5
Akram Injaileh <159902782+akraminjaileh@users.noreply.github.com>
2024-02-15 18:40:22 +0300

    Update CreateBookDTO.cs

--- E-Libriray-API/DTOs/CreateBookDTO.cs
+++ E-Libriray-API/DTOs/CreateBookDTO.cs
@@ -2,7 +2,7 @@
 {
     public class CreateBookDTO
     {
-        public int id { get; set; }
+        public int BookID { get; set; }
         public string ? titel { get; set; }
         public float ? price { get; set; }
         public string ? description { get; set; }
